b8d338f6f0cd5c6afd50ba533bd4f4badc4ef69e
danielliflash12 <dli1698@gmail.com>
2023-11-04 17:47:40 -0500

    commit test

---
+++ lstm.py
@@ -0,0 +1,8 @@
+pip install tensorflow
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.optimizers import Adam
+
+model = Sequential()
+
